0e63d1a5048254c5f391c64e4aa690874f4957e4
Don Heshanthaka <heshanthaka007@gmail.com>
2022-01-09 20:10:56 +0530

    insert, search and getNewNode functions have been implemented

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct bstNode{
     int data;
@@ -7,7 +8,64 @@ struct bstNode{
     struct bstNode *right;
 };
 
+// function creates a new node in the heap
+struct bstNode *getNewNode(int data){
+    struct bstNode *newNode = malloc(sizeof(struct bstNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+};
+
+// A recursive function to insert data into the tree
+struct bstNode *insert(struct bstNode *root, int data){
+    if(root == NULL){ // if the tree is empty
+        root = getNewNode(data);
+
+    }else if( data <= root->data){ // if the data is less than the data in the current node, insert to the left subtree
+        root->left = insert(root->left, data);
+
+    }else{ // if the data is greater than the data in the current node, insert to the right subtree
+        root->right = insert(root->right, data);
+    }
+    return root;
+
+};
+
+bool search(struct bstNode *root, int data){
+    if(root == NULL){
+        return false;
+    }
+    else if(root->data == data){
+        return true;
+    }
+    else if(data <= root->data){
+        return search(root->left, data);
+    }
+    else{
+        return search(root->right, data);
+    }
+}
+
 int main()
 {
+
+    struct BstNode *root = NULL;  // Creating an empty tree
+
+	root = insert(root,15);
+	root = insert(root,10);
+	root = insert(root,20);
+	root = insert(root,25);
+	root = insert(root,8);
+	root = insert(root,12);
+
+	int number = 20;
+	if(search(root,number) == true){
+        printf("Found!\n");
+	}
+	else{
+        printf("Not found!\n");
+	}
+
     return 0;
 }
